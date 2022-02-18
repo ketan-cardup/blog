8887b7c478d91339a0b1f24bb4d762ac8cab5b5e
Ketan Deshmukh <ketandeshmukh@cardup.co>
2022-02-18 18:45:22 +0530

    Tryout for Github Actions user

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,10 @@ set :user,            'deploy'
 set :branch,          'main'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-
 set :pty, true
 set :use_sudo, true
 set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
+
+server ENV.fetch('SERVER_IP')
+
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,15 +7,15 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server ENV.fetch('SERVER_IP'),
-	user: 'deploy',
-	roles: %w{web db app},
-	ssh_options: {
-		keys: [ENV.fetch('SERVER_SSH_KEY')],
-    keys_only: true,
-		auth_methods: %w(publickey),
-    forward_agent: true
-	}
+# server ENV.fetch('SERVER_IP'),
+# 	user: 'deploy',
+# 	roles: %w{web db app},
+# 	ssh_options: {
+# 		keys: [ENV.fetch('SERVER_SSH_KEY')],
+#     keys_only: true,
+# 		auth_methods: %w(publickey),
+#     forward_agent: true
+# 	}
 
 # role-based syntax
 # ==================
