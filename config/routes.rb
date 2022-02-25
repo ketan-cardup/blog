901f5fbe42f69013315d5089c797782730a2cc5d
Ketan Deshmukh <ketandeshmukh@cardup.co>
2022-02-25 12:11:19 +0530

    Added some basic assets for testing with cloudfront

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts do 
+  resources :posts do
     resources :comments
-  end 
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
 end
